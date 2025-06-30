d95a519ccd7bba272fcd5e64fc72d450721f3957
Robert Crandall <robert-crandall@github.com>
2025-06-29 20:57:06 -0700

    Formatting

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { 
+	kit: {
 		adapter: adapter({
 			// SPA mode: serve index.html for all routes
 			fallback: 'index.html',
